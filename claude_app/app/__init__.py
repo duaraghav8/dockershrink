6a70270664d0d9cb1b04750331daa7c755a011b3
duaraghav8 <duaraghav8@protonmail.com>
2024-09-17 20:06:47 +0200

    fix login view name

--- claude_app/app/__init__.py
+++ claude_app/app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.login'
     oauth.init_app(app)
 
     # OAuth configuration
