cb17e7141f512f998942905a25dfaceafda5c564
Raghav <duaraghav8@protonmail.com>
2025-01-17 00:26:07 +0100

    refine logging

--- internal/log/log.go
+++ internal/log/log.go
@@ -8,8 +8,6 @@ import (
 // TODO
 // - remove logger metadata from log messages, just keep the message
 // - debug logs should be gray color to signify background stuff
-// - data should be map[string]string, print everything as key=value
-//   - if some value is too long, print it in a separate line and give enough whitespace in between
 
 type Logger struct {
 	debugEnabled bool
@@ -20,32 +18,36 @@ func NewLogger(debug bool) *Logger {
 	return &Logger{debugEnabled: debug}
 }
 
-func (l *Logger) Debug(msg string, data map[string]interface{}) {
+func (l *Logger) Debug(msg string, data map[string]string) {
 	if l.debugEnabled {
 		l.printLog("DEBUG", msg, data)
 	}
 }
 
-func (l *Logger) Info(msg string, data map[string]interface{}) {
+func (l *Logger) Info(msg string, data map[string]string) {
 	l.printLog("INFO", msg, data)
 }
 
-func (l *Logger) Warn(msg string, data map[string]interface{}) {
+func (l *Logger) Warn(msg string, data map[string]string) {
 	l.printLog("WARN", msg, data)
 }
 
-func (l *Logger) Error(msg string, data map[string]interface{}) {
+func (l *Logger) Error(msg string, data map[string]string) {
 	l.printLog("ERROR", msg, data)
 }
 
-func (l *Logger) Fatal(msg string, data map[string]interface{}) {
+func (l *Logger) Fatal(msg string, data map[string]string) {
 	l.printLog("FATAL", msg, data)
 	os.Exit(1)
 }
 
-func (l *Logger) printLog(level, msg string, data map[string]interface{}) {
+func (l *Logger) printLog(level, msg string, data map[string]string) {
 	if data != nil && len(data) > 0 {
-		log.Printf("[%s] %s | %v\n", level, msg, data)
+		log.Printf("[%s] %s\n", level, msg)
+		for k, v := range data {
+			log.Printf("    %s=%s", k, v)
+		}
+		log.Println("")
 	} else {
 		log.Printf("[%s] %s\n", level, msg)
 	}
